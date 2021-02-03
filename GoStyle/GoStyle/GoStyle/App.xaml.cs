65c25946175f0362172809d1d19dbbefee370a9b
shaossword <45566987+shaossword@users.noreply.github.com>
2021-02-03 16:52:04 +0100

    start + qrcode

---
+++ GoStyle/GoStyle/GoStyle/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoStyle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("start");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("start - " + id);
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("sleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("resume");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("resume - " + id);
+            }
+        }
+    }
+}
---
+++ GoStyle/GoStyle/GoStyle/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoStyle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnClickListPromo(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "PROMO";
+
+            var dateDuJour = DateTime.Now.ToString("F");
+
+            await Navigation.PushAsync(new ListPromo(dateDuJour));
+        }
+
+        private void OnPressListPromo(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            BtnListPromo.BackgroundColor = Color.LightBlue;
+        }
+
+        private void OnReleaseListPromo(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            BtnListPromo.BackgroundColor = Color.FromHex("#2196F3");
+        }
+    }
+}
